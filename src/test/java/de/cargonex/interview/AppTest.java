f7e73bfc8f506fa1031392446e8fa6077a778b8b
anydef <fed.pavlo+github@gmail.com>
2021-03-30 13:27:39 +0200

    init

---
+++ src/test/java/de/cargonex/interview/AppTest.java
@@ -0,0 +1,11 @@
+package de.cargonex.interview;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.hello(), "app should have a greeting");
+    }
+}
